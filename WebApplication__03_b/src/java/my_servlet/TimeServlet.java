688c24113618c3e6a3bad574db6fd42ffb8c0e3c
MinKoThichCode <lequocminh6440@gmail.com>
2025-01-13 11:34:00 +0700

    WebApplication _03_d

---
+++ WebApplication__03_b/src/java/my_servlet/TimeServlet.java
@@ -0,0 +1,58 @@
+package my_servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ADMIN
+ */
+@WebServlet(name = "TimeServlet", urlPatterns = {"/TimeServlet"})
+public class TimeServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        // Lấy thời gian hiện tại
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedTime = now.format(formatter);
+
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet TimeServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Thời gian hiện tại là: " + formattedTime + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Servlet hiển thị thời gian hiện tại";
+    }
+}
